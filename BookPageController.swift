b03bdbe3b2852b63157e970a952aba1ab623d9ca
Aswin Raj Kharel <query.ark@gmail.com>
2018-09-20 17:32:43 -0500

    Transform Cells to Pages

--- BookPageController.swift
+++ BookPageController.swift
@@ -8,21 +8,36 @@
 
 import UIKit
 
-class BookPageController: UICollectionViewController{
+class BookPageController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView?.backgroundColor = .white
         self.navigationItem.title = "Book"
         self.collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellID")
+        
+        // Collectionview layout
+        let layout = self.collectionView?.collectionViewLayout as? UICollectionViewFlowLayout
+        layout?.scrollDirection = .horizontal
+        self.collectionView?.isPagingEnabled = true
+        layout?.minimumLineSpacing = 0
     }
     
+    // Configuring cell size - conform to UICOllectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: self.view.frame.height)
+    }
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 30
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
-        cell.backgroundColor = .red
-        return cell
+        if indexPath.item % 2 == 0 {
+            cell.backgroundColor = .blue
+        }else{
+            cell.backgroundColor = .red
+        }
+            return cell
     }
 }
