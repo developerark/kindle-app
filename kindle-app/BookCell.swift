930d9b6e91f39e5076f695e9b5eccb22d6d9fc51
Aswin Raj Kharel <query.ark@gmail.com>
2018-09-19 20:09:59 -0500

    Auto Layout

--- kindle-app/BookCell.swift
+++ kindle-app/BookCell.swift
@@ -12,11 +12,13 @@ class BookCell: UITableViewCell{
     let coverImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = .red
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     let titleLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "This is a text for the label"
         label.backgroundColor = .blue
         return label
@@ -24,20 +26,37 @@ class BookCell: UITableViewCell{
     
     let authorLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "This is the author label"
+        label.backgroundColor = .green
         return label
     }()
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         print("Cell is being Initialized")
-        self.backgroundColor = .yellow
+        
+        // Adding coverImageView and constraints
         self.addSubview(self.coverImageView)
-        self.coverImageView.frame = CGRect(x: 8, y: 8, width: 50, height: 64)
+        self.coverImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        self.coverImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        self.coverImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8).isActive = true
+        self.coverImageView.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        
+        // Adding titleLabel and constraints
         self.addSubview(self.titleLabel)
-        self.titleLabel.frame = CGRect(x: 66, y: 20, width: 200, height: 20)
+        self.titleLabel.leftAnchor.constraint(equalTo: self.coverImageView.rightAnchor, constant: 8).isActive = true
+        self.titleLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        self.titleLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        self.titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -20).isActive = true
+        
+        // Adding authorLabel and constraints
         self.addSubview(self.authorLabel)
-        self.authorLabel.frame = CGRect(x: 66, y: 48, width: 200, height: 20)
+        self.authorLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 4).isActive = true
+        self.authorLabel.leftAnchor.constraint(equalTo: self.coverImageView.rightAnchor, constant: 8).isActive = true
+        self.authorLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        self.authorLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
